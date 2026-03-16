5637b5406bea93064ee06d103445dec9482786e9
matteolpera <matteo.lepera02@gmail.com>
2026-03-16 15:13:59 +0100

    feat: rimossa la pagina welcome.
    aggiunta pagina home page.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Laravel Page</title>
+</head>
+
+<body>
+    <h1>Home Page</h1>
+
+    <div>
+        <h3>Questa è una bella pagina</h3>
+        <p>Sei finito qui per caso o per cosa?</p>
+    </div>
+</body>
+
+</html>
